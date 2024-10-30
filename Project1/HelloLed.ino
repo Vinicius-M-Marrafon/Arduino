a3e4ffad66fc32bc23eb85886ccfc6d14f321db5
Vinicius M. Marrafon <96776865+Vinicius-M-Marrafon@users.noreply.github.com>
2024-10-30 16:53:19 -0300

    Update HelloLed.ino

--- Project1/HelloLed.ino
+++ Project1/HelloLed.ino
@@ -1,1 +1,14 @@
+void setup() {
+  // put your setup code here, to run once:
+  // Define pin 13 as output
+  pinMode(12, OUTPUT); 
+}
 
+// For 
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(12, HIGH);
+  delay(900);
+  digitalWrite(12, LOW);
+  delay(100);
+}
